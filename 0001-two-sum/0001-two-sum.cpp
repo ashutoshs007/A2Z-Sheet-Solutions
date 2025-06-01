ef2566d00518b5d45aa6b025cfaf0000fe429261
ashutoshs007 <ashutosh1408singh@gmail.com>
2025-06-01 19:23:53 +0530

    Time: 3 ms (72.03%), Space: 14.8 MB (50.76%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -4,12 +4,13 @@ public:
         int n=nums.size();
         unordered_map<int,int>mpp;
         for(int i=0;i<n;i++){
-            int more =target-nums[i];
+            int more=target-nums[i];
             if(mpp.find(more)!=mpp.end()){
                 return{mpp[more],i};
             }
             mpp[nums[i]]=i;
         }
         return {-1,-1};
+        
     }
 };

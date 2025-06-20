9ee34e7e3f41527dba55d2c98473a68b3d55df88
ashutoshs007 <ashutosh1408singh@gmail.com>
2025-06-20 19:08:36 +0530

    Time: 3 ms (89.63%), Space: 23.9 MB (33.34%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.cpp
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -1,35 +1,31 @@
 class Solution {
 public:
-    vector<vector<int>> merge(vector<vector<int>>& arr) {
-        int n=arr.size();
-        sort(arr.begin(),arr.end());
+    vector<vector<int>> merge(vector<vector<int>>& nums) {
+        int n=nums.size();
+        sort(nums.begin(),nums.end());
         vector<vector<int>>ans;
-
-        // Select an interval
         for(int i=0;i<n;i++){
-        int start=arr[i][0];
-        int end=arr[i][1];
+            int start=nums[i][0];
+            int end=nums[i][1];
+            /*while traversing in the sorted interval
+            when we go to (2,4) we saw that it is already in (1,6) 
+            so we did not do anything for it 
+            so go and check ,same code for this is in the below line*,if end of (2,4) lies in (1,6) then continue*/
 
-        //Skip all merged intervals:
-        if(!ans.empty()&& end<= ans.back()[1]){
-            continue;
-        }
-
-        //check the rest of intervals
-        for(int j=i+1;j<n;j++){
-            if(arr[j][0]<=end){
-                end=max(end,arr[j][1]);
+            if(!ans.empty()&& end<=ans.back()[1]){
+                continue;
             }
-
-            else{
-                break;
+            for(int j=i+1;j<n;j++){
+                if(nums[j][0]<=end){
+                    end=max(end,nums[j][1]);
+                }
+                else{
+                    break;
+                }
             }
+          ans.push_back({start,end});
+ 
         }
-        ans.push_back({start,end});
-
-        }
-        return ans;
-
-
+    return ans;
     }
 };

d3faf7d7749c509e5cbea04d1e68511f3033912f
ashutoshs007 <ashutosh1408singh@gmail.com>
2025-06-20 21:13:37 +0530

    Time: 4 ms (69.4%), Space: 23.7 MB (80.22%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.cpp
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -5,27 +5,13 @@ public:
         sort(nums.begin(),nums.end());
         vector<vector<int>>ans;
         for(int i=0;i<n;i++){
-            int start=nums[i][0];
-            int end=nums[i][1];
-            /*while traversing in the sorted interval
-            when we go to (2,4) we saw that it is already in (1,6) 
-            so we did not do anything for it 
-            so go and check ,same code for this is in the below line*,if end of (2,4) lies in (1,6) then continue*/
-
-            if(!ans.empty()&& end<=ans.back()[1]){
-                continue;
-            }
-            for(int j=i+1;j<n;j++){
-                if(nums[j][0]<=end){
-                    end=max(end,nums[j][1]);
-                }
-                else{
-                    break;
-                }
-            }
-          ans.push_back({start,end});
- 
+      if(ans.empty() ||nums[i][0] >ans.back()[1]){
+      ans.push_back(nums[i]);
+}
+else{
+    ans.back()[1]=max(nums[i][1],ans.back()[1]);
+}
         }
-    return ans;
+        return ans;
     }
 };

52e0bdf09727aab0d2209b82462cdfdffb258003
ashutoshs007 <ashutosh1408singh@gmail.com>
2025-06-24 11:56:48 +0530

    Time: 6 ms (97.19%), Space: 58.1 MB (74.95%) - LeetHub

---
+++ 0148-sort-list/0148-sort-list.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    ListNode* sortList(ListNode* head) {
+        if (!head || !head->next) return head;
+
+        vector<int> arr;
+        ListNode* temp = head;
+
+        // Extract values into array
+        while (temp != nullptr) {
+            arr.push_back(temp->val);  // ❌ fixed from head->data to temp->val
+            temp = temp->next;
+        }
+
+        // Sort the array
+        sort(arr.begin(), arr.end());
+
+        // Rewrite values back to the list
+        temp = head;
+        for (int i = 0; i < arr.size(); i++) {
+            temp->val = arr[i];  // ❌ fixed from temp->data to temp->val
+            temp = temp->next;
+        }
+
+        return head;
+    }
+};

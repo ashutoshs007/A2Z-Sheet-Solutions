4c6e320209d041a54a7c7b9246b2f78f13bcc516
ashutoshs007 <ashutosh1408singh@gmail.com>
2025-08-14 23:35:57 +0530

    Time: 0 ms (100%), Space: 12.6 MB (8.6%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.cpp
+++ 0088-merge-sorted-array/0088-merge-sorted-array.cpp
@@ -1,28 +1,29 @@
 class Solution {
 public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-        vector<int> arr3(m + n);
-        int left = 0, right = 0, index = 0;
+        vector<int>nums3(m+n);
 
-        while (left < m && right < n) {
-            if (nums1[left] <= nums2[right]) {
-                arr3[index++] = nums1[left++];
-            } else {
-                arr3[index++] = nums2[right++];
+        int left=0, right=0,index=0;
+        while(left<m && right<n){
+            if(nums1[left]<=nums2[right]){
+               nums3[index++]=nums1[left++];
             }
-        }
+            else{
+                nums3[index++]=nums2[right++];
+            }
+       }
 
-        while (left < m) {
-            arr3[index++] = nums1[left++];
-        }
+       while(left<m){
+        nums3[index++]=nums1[left++];
+       }
+       while(right<n){
+        nums3[index++]=nums2[right++];
+       }
+       
 
-        while (right < n) {
-            arr3[index++] = nums2[right++];
-        }
-
-        // Copy merged result back to nums1
-        for (int i = 0; i < m + n; i++) {
-            nums1[i] = arr3[i];
-        }
+    for(int i=0;i<m+n;i++){
+        nums1[i]=nums3[i];
+    }
+        
     }
 };
